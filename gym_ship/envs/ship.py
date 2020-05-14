06f6ea001929d28e0ec73d595a5916542aecb2c4
Ernie <ernie_hs@HAL9000.localdomain>
2020-05-14 18:29:28 +0200

    added a ship and a little fizix

---
+++ gym_ship/envs/ship.py
@@ -0,0 +1,13 @@
+import pyglet
+from pyglet import gl
+
+class Ship:
+
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+    def destroy(self):
+        pass
--- gym_ship/envs/ship_env.py
+++ gym_ship/envs/ship_env.py
@@ -1,68 +1,99 @@
+import sys
+import math
+import numpy as np
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import colorize, seeding, EzPickle
-import pyglet
-from pyglet import gl
+
+import Box2D
+from Box2D.b2 import (edgeShape, circleShape, fixtureDef,
+                      polygonShape, revoluteJointDef, contactListener)
 
 WINDOW_W = 1024
 WINDOW_H = 768
 FPS = 60
 
+SHIP_POLY = [(-5, 0), (0, 5), (5, 0), (5, -20), (-5, -20)]
+
+
+class ContactDetector(contactListener):
+    def __init__(self, env):
+        contactListener.__init__(self)
+        self.env = env
+
+    def BeginContact(self, contact):
+        pass
+
+    def EndContact(self, contact):
+        pass
+
+
 class ShipEnv(gym.Env, EzPickle):
-    
-    metadata = {'render.modes': ['human'],
-            'video.frames_per_second' : FPS}
+
+    metadata = {'render.modes': ['human', 'rgb_array'],
+                'video.frames_per_second': FPS}
 
     def __init__(self):
         EzPickle.__init__(self)
         self.viewer = None
-    
+        self.world = Box2D.b2World((0, 0), doSleep=True)
+        self.ship = None
+        self.reset()
+
     def step(self, action):
-        pass
-    
+        reward = 0
+        done = False
+        state = [0, 0]
+
+        self.ship.ApplyForce(
+            (0, 10), (self.ship.position[0] + 1, self.ship.position[1]), True)
+
+        self.world.Step(1.0/FPS, 6, 2)
+        return np.array(state, dtype=np.float32), reward, done, {}
+
     def _destroy(self):
-        pass
+        if not self.ship:
+            return
+        self.world.contactListener = None
+        self.world.DestroyBody(self.ship)
+        self.ship = None
 
     def reset(self):
         self._destroy()
+        self.world.contactListener = ContactDetector(self)
+
+        self.ship = self.world.CreateDynamicBody(
+            position=(100, 100),
+            angle=0,
+            fixtures=fixtureDef(
+                shape=polygonShape(vertices=SHIP_POLY),
+                density=1.0,
+                friction=0.5,
+                restitution=0.1
+            )
+        )
+        self.ship.color1 = (1.0, 0, 0)
+        self.ship.color2 = (0, 0, 0)
+
+        self.drawlist = [self.ship]
+        return self.step(np.array([0, 0], dtype=np.float32))
 
     def render(self, mode='human'):
-        assert mode in ['human']
+        from gym.envs.classic_control import rendering
         if self.viewer is None:
-            from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(WINDOW_W, WINDOW_H)
-            self.transform = rendering.Transform()
-        win = self.viewer.window
-        win.switch_to()
-        win.dispatch_events()
-        win.clear()
-        pixel_scale = 1
-        if hasattr(win.context, '_nscontext'):
-            pixel_scale = win.context._nscontext.view().backingScaleFactor()  
-        VP_W = int(pixel_scale * WINDOW_W)
-        VP_H = int(pixel_scale * WINDOW_H) 
+            self.viewer.set_bounds(0, WINDOW_W, 0, WINDOW_H)
 
-        t = self.transform
-        gl.glViewport(0, 0, VP_W, VP_H)
-        t.enable()
+        for obj in self.drawlist:
+            for f in obj.fixtures:
+                trans = f.body.transform
+                path = [trans * v for v in f.shape.vertices]
+                self.viewer.draw_polygon(path, color=obj.color1)
+                path.append(path[0])
+                self.viewer.draw_polyline(path, color=obj.color2, linewidth=1)
 
-        gl.glBegin(gl.GL_QUADS)
-        gl.glVertex2f(100, 100)
-        gl.glVertex2f(200, 100)
-        gl.glVertex2f(200, 200)
-        gl.glVertex2f(100, 200)
-        gl.glEnd()
-
-        t.disable()
-        
-        if mode == 'human':
-            win.flip()
-            return self.viewer.isopen
-        image_data = pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
-        arr = np.fromstring(image_data.get_data(), dtype=np.uint8, sep='')
-        arr = arr.reshape(VP_H, VP_W, 4)
-        arr = arr[::-1, :, 0:3]
-        return arr
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
     def close(self):
         if self.viewer is not None:
@@ -70,16 +101,20 @@ class ShipEnv(gym.Env, EzPickle):
             self.viewer = None
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
     env = ShipEnv()
-    env.render()
-    isopen = True
-    while isopen:
-        env.reset()
-        while True:
-            isopen = env.render()
-            if isopen == False:
-                break
+    total_reward = 0
+    steps = 0
+    s = env.reset()
+    while True:
+        s, r, done, info = env.step(np.array([0, 0], dtype=np.float32))
+        total_reward += r
+        if env.render() == False:
+            break
+        if steps % 20 == 0 or done:
+            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
+            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+        steps += 1
+        if done:
+            break
     env.close()
-
